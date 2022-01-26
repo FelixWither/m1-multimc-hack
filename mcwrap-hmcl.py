f35fb65e6f243fb1924c559339cb450ff270b2ed
FelixWither <77829265+FelixWither@users.noreply.github.com>
2022-01-26 17:28:31 +0800

    Add files via upload
    Modified 'LWJGL version check' section of code to be compatible with HMCL's folder structure.

---
+++ mcwrap-hmcl.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+import glob
+import pathlib
+import logging
+import shutil
+import json
+from os import listdir
+from os.path import isfile, join
+
+LOG_FILE='/tmp/mcwrap.log'
+logging.getLogger().addHandler(logging.FileHandler(LOG_FILE))
+GLlist = []
+
+def lwjglver():
+    inst_dir = os.environ["INST_DIR"]
+    gameVersion = inst_dir[inst_dir.find('1.'):].replace('/', '')
+    hmcl_config = json.load(open(os.path.join(inst_dir, gameVersion + '.json')))
+    for entries in hmcl_config['libraries']:
+        for keys in entries:
+            if str(entries[keys]).find('lwjgl') != -1:
+                if not isinstance((entries[keys]), dict):
+                    if str(entries[keys]).find('lwjgl:2.') != -1:
+                        GLlist.append(str(entries[keys]))
+                    elif str(entries[keys]).find('lwjgl:3.') != -1:
+                        GLlist.append(str(entries[keys]))
+    lwjgl_ver = GLlist[0]
+    if lwjgl_ver == 'org.lwjgl.lwjgl:lwjgl:2.9.4-nightly-20150209':
+        # LWJGL 2.9.4 nightly on Feb 9, 2015
+        return 'lwjgl'
+    elif '3' in lwjgl_ver:
+        # Use Tanmay's natives
+        return 'lwjgl3'
+    
+def this_dir():
+    return os.path.dirname(os.path.abspath(__file__))
+
+
+def lwjgl_jar_path():
+    lwjglclasspath = os.path.join(this_dir(), lwjglver() + 'classpath')
+    lwjgljars = [f for f in os.listdir(lwjglclasspath) if isfile(os.path.join(lwjglclasspath, f))]
+    out = []
+    for jar in lwjgljars:
+        out.append(os.path.join(lwjglclasspath, jar))
+    return out
+
+
+def m1_native_libs_dir():
+    return os.path.join(this_dir(), lwjglver() + 'natives')
+
+
+def copy_native_libs(dest_dir):
+    shutil.rmtree(dest_dir)
+    pathlib.Path(dest_dir).mkdir(parents=True, exist_ok=True)
+    logging.info('copying native libs from {} to {}'.format(m1_native_libs_dir(), dest_dir))
+    for f in glob.glob('{}/*.dylib'.format(m1_native_libs_dir())):
+        logging.info('copying {}'.format(os.path.basename(f)))
+        shutil.copy(f, dest_dir)
+
+
+def rewrite_classpath(cp):
+    jars = [j for j in cp.split(':')]
+    for jar in lwjgl_jar_path():
+        jars.insert(0, jar)
+    logging.info('rewritten classpath: {}'.format(jars))
+    return ':'.join(jars)
+
+
+def rewrite_mc_args(mc_args):
+    out = []
+    for a in mc_args:
+        if 'lwjgl' in a:
+            if not '-D' in a:
+                a = rewrite_classpath(a)
+        logging.info('arg: {}'.format(a))
+        out.append(a)
+    return out
+
+
+def launch_mc(mc_args, env_vars="none"):
+    logging.info('running minecraft with args: {}'.format(mc_args))
+    logging.info('and with env vars: {}'.format(env_vars))
+    if env_vars != "none":
+        subprocess.run(mc_args, env=env_vars)
+    else:
+        subprocess.run(mc_args)
+
+def instance_dir():
+    return os.environ['INST_DIR']
+
+
+def natives_dir():
+    return os.path.join(instance_dir(), 'natives')
+
+
+def run():
+    mc_args = rewrite_mc_args(sys.argv[1:])
+
+    copy_native_libs(natives_dir())
+    env_vars={"":""}
+    if env_vars != {"":""}:
+        launch_mc(mc_args, env_vars)
+    else:
+        launch_mc(mc_args)
+
+if __name__ == '__main__':
+    run()
